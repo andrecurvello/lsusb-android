c652c3e3e1301291b6f365a8c4a0f4492789cefa
chrulri <chrulri@gmail.com>
2012-08-12 18:28:58 +0200

    GPL license

---
+++ README.md
@@ -0,0 +1,35 @@
+lsusb - Android
+===============
+
+Graphical [USB Host][1] Device List for Android
+
+It is available on Google Play for free: [lsusb][2]
+
+
+Feel free to fork, create issues, [buy me a beer][3] or just having fun with this tool!
+
+If you fork it, feel free to send me pull requests for your awesome new features so they'll make it on the market.
+
+LICENSE
+=======
+ ******************************************************************************
+	lsusb, an open source graphical USB host device list for Android
+	Copyright (C) 2012  Christian Ulrich <chrulri@gmail.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+
+[1]: https://developer.android.com/guide/topics/connectivity/usb/index.html 
+[2]: https://play.google.com/store/apps/details?id=com.chrulri.lsusb
+[3]: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=chrulri@gmail.com&item_name=lsusb-android
--- src/com/chrulri/lsusb/MainActivity.java
+++ src/com/chrulri/lsusb/MainActivity.java
@@ -1,9 +1,27 @@
+/******************************************************************************
+ *  lsusb, an open source graphical USB host device list for Android          *
+ *  Copyright (C) 2012  Christian Ulrich <chrulri@gmail.com>                  *
+ *                                                                            *
+ *  This program is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  This program is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+ ******************************************************************************/
 
 package com.chrulri.lsusb;
 
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends ListActivity {
 
@@ -12,9 +30,7 @@ public class MainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         adapter = new UsbAdapter(this);
-        adapter.loadDevices();
         setListAdapter(adapter);
     }
 
@@ -28,6 +44,17 @@ public class MainActivity extends ListActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                adapter.loadDevices();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
